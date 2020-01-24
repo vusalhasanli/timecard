ff2d4c2597c0491b3a82cf5ded121b92f5987d9d
vusalhasanli <v.m.hasanzade@gmail.com>
2020-01-24 14:26:57 -0800

    minor changes

--- timecard.py
+++ timecard.py
@@ -3,46 +3,31 @@ from selenium import webdriver
 import json
 
 
-
-
-
 class Webdriver:
-    
-
     driver = webdriver.Firefox()
     def login(self, user_id, password):
-
         """
-
         logs into westvalley using user's id and password.
-
         """
         with open ('./main.json') as main_file:
             data = main_file.read()
             obj = json.loads(data)
-        print(str(obj['user_id']))
-
+        # print(str(obj['user_id']))
         self.driver.get("https://westvalley.fastime.com")
         self.driver.find_element_by_id("txtOrderNumber").send_keys(str(obj['user_id']))
         self.driver.find_element_by_name("txtSSN").send_keys(str(obj['password']))
         self.driver.find_element_by_id("btnSubmit").click()
         self.driver.find_element_by_id("btnAccept").click()
     def save_for_later(self):
-
         """
-
         saves after inserting times.
-
         """
         self.driver.find_element_by_id("btnReview").click()
-        self.driver.find_element_by_id("saveLater").click()
+        # self.driver.find_element_by_id("saveLater").click()
         self.driver.switch_to.alert.accept()
     def day_fill(self, day, code_in, code_out):
-
         """
-
         fills the times for each work day.
-
         """
         self.driver.find_element_by_id("TCC_txt_{}_{}_IN".format(code_in,day)).send_keys("09:00")
         self.driver.find_element_by_id("TCC_optAMPM_{}_{}_IN".format(code_in, day)).send_keys("AM")
@@ -52,7 +37,6 @@ class Webdriver:
         self.driver.find_element_by_id("TCC_optAMPM_{}_{}_IN".format(code_out,day)).send_keys("PM")
         self.driver.find_element_by_id("TCC_txt_{}_{}_OUT".format(code_out,day)).send_keys("05:30")
         self.driver.find_element_by_id("TCC_optAMPM_{}_{}_OUT".format(code_out,day)).send_keys("PM")
-
 def main():
     date_codes = {
         'MON': ['6_0', '6_1'],
@@ -64,13 +48,8 @@ def main():
     westvalley = Webdriver()
     westvalley.login('user_id', 'password')
     for day, value in date_codes.items():
-
         westvalley.day_fill(day, value[0], value[1])
-
-
     westvalley.save_for_later()
-
 if __name__ == '__main__':
-
     main()
 
