1a2c3e1575b0036626ba091ead20d8687b120438
vusalhasanli <v.m.hasanzade@gmail.com>
2020-02-07 16:47:08 -0800

    added check days off

--- timecard.py
+++ timecard.py
@@ -9,11 +9,7 @@ class Webdriver:
         """
         logs into westvalley using user's id and password.
         """
-        # with open ('./main.json') as main_file:
-        #     data = main_file.read()
-        #     obj = json.loads(data)
-        # print(str(obj['user_id']))
-        user_id = os.environ.get('USER_ID')
+        user_id = os.environ.get('USER_ID') 
         password = os.environ.get('PASSWORD')
         self.driver.get("https://westvalley.fastime.com")
         self.driver.find_element_by_id("txtOrderNumber").send_keys(user_id)
@@ -26,32 +22,54 @@ class Webdriver:
         """
         self.driver.find_element_by_id("btnReview").click()
         # self.driver.find_element_by_id("saveLater").click()
-        self.driver.switch_to.alert.accept()
-    def day_fill(self, day, code_in, code_out):
+        # self.driver.switch_to.alert.accept()
+    def day_fill(self, day, code_in, code_out, date_code, holidays):
         """
         fills the times for each work day.
         """
-        self.driver.find_element_by_id("TCC_txt_{}_{}_IN".format(code_in,day)).send_keys("09:00")
-        self.driver.find_element_by_id("TCC_optAMPM_{}_{}_IN".format(code_in, day)).send_keys("AM")
-        self.driver.find_element_by_id("TCC_txt_{}_{}_OUT".format(code_in,day)).send_keys("12:00")
-        self.driver.find_element_by_id("TCC_optAMPM_{}_{}_OUT".format(code_in,day)).send_keys("PM")
-        self.driver.find_element_by_id("TCC_txt_{}_{}_IN".format(code_out,day)).send_keys("12:30")
-        self.driver.find_element_by_id("TCC_optAMPM_{}_{}_IN".format(code_out,day)).send_keys("PM")
-        self.driver.find_element_by_id("TCC_txt_{}_{}_OUT".format(code_out,day)).send_keys("05:30")
-        self.driver.find_element_by_id("TCC_optAMPM_{}_{}_OUT".format(code_out,day)).send_keys("PM")
+        date = self.driver.find_element_by_xpath('//*[@id="Table13"]/tbody/tr[{}]/td[5]/font'.format(date_code)).text.strip()
+        days_off = holidays['day_off']
+
+        if date in days_off: 
+            print('{} is a holiday. skipping'.format(date))
+        else:
+            self.driver.find_element_by_id("TCC_txt_{}_{}_IN".format(code_in,day)).send_keys("09:00")
+            self.driver.find_element_by_id("TCC_optAMPM_{}_{}_IN".format(code_in, day)).send_keys("AM")
+            self.driver.find_element_by_id("TCC_txt_{}_{}_OUT".format(code_in,day)).send_keys("12:00")
+            self.driver.find_element_by_id("TCC_optAMPM_{}_{}_OUT".format(code_in,day)).send_keys("PM")
+            self.driver.find_element_by_id("TCC_txt_{}_{}_IN".format(code_out,day)).send_keys("12:30")
+            self.driver.find_element_by_id("TCC_optAMPM_{}_{}_IN".format(code_out,day)).send_keys("PM")
+            self.driver.find_element_by_id("TCC_txt_{}_{}_OUT".format(code_out,day)).send_keys("05:30")
+            self.driver.find_element_by_id("TCC_optAMPM_{}_{}_OUT".format(code_out,day)).send_keys("PM")
 def main():
     date_codes = {
-        'MON': ['6_0', '6_1'],
-        'TUE': ['5_0', '5_1'],
-        'WED': ['4_0', '4_1'],
-        'THU': ['3_0', '3_1'],
-        'FRI': ['2_0', '2_1']
+        'MON': ['6_0', '6_1', '4'],
+        'TUE': ['5_0', '5_1', '7'],
+        'WED': ['4_0', '4_1', '10'],
+        'THU': ['3_0', '3_1', '13'],
+        'FRI': ['2_0', '2_1', '16']
+    }
+    holidays = {
+        'day_off': [
+            '1/01',
+            '1/20',
+            '2/17',
+            '5/25',
+            '7/03',
+            '9/07',
+            '11/26',
+            '12/24',
+            '12/25',
+            '12/31',
+            '1/30',
+        ]
     }
     westvalley = Webdriver()
     westvalley.login('user_id', 'password')
     for day, value in date_codes.items():
-        westvalley.day_fill(day, value[0], value[1])
+        westvalley.day_fill(day, value[0], value[1], value[2], holidays)
+            
+        
     westvalley.save_for_later()
 if __name__ == '__main__':
     main()
-
